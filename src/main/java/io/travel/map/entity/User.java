9454b04aa3f20fdb667e5701d5de22d319012587
Jake6654 <ugw2ad@virginia.edu>
2025-03-25 20:55:51 -0400

    DB Test

--- src/main/java/io/travel/map/entity/User.java
+++ src/main/java/io/travel/map/entity/User.java
@@ -4,27 +4,48 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@Table(name = "users")
+@Document(collection = "users")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String  id;
 
-    @Column(nullable = false, unique = true)
     private String email;
-
-    @Column(nullable = false)
     private String name;
-
-    @Column(nullable = false)
     private String photoUrl;
 
 
+    public String getId() {
+        return id;
+    }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
 }
--- src/main/java/io/travel/map/repository/UserRepository.java
+++ src/main/java/io/travel/map/repository/UserRepository.java
@@ -4,12 +4,12 @@ package io.travel.map.repository;
 
 import io.travel.map.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends MongoRepository<User, String > {
     Optional<User> findByEmail(String email); // 이메일로 유저 검색
-
 }
